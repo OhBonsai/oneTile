ae97c09c81ecfdde10ff1b7b8d6e154c9109d34e
bonsai <LetBonsaiBe@gmail.com>
2016-09-08 17:03:03 +0800

    add circle vertex generator function ARC

--- js/app/core/math.js
+++ js/app/core/math.js
@@ -44,6 +44,7 @@ define(['glm'], function (glm) {
 
 
     return {
+        // get normal direction to increase line width
         getNormals : function(points, closed){
             var lineA = [0, 0];
             var lineB = [0, 0];
@@ -103,8 +104,55 @@ define(['glm'], function (glm) {
             }
 
             return out
-        }
+        },
+        
+        // out circle path point
+        arc: function(x, y, radius, start, end, clockwise, steps, path) {
+            if (!path){
+                path = [];
+            }
+                
 
+            x = x||0;
+            y = y||0;
+            radius = radius||0;
+            start = start||0;
+            end = end||0;
+
+            //determine distance between the two angles
+            //...probably a nicer way of writing this
+            var dist = Math.abs(start-end);
+            if (!clockwise && start > end)
+                dist = 2*Math.PI - dist;
+            else if (clockwise && end > start)
+                dist = 2*Math.PI - dist;
+
+            //approximate the # of steps using the cube root of the radius
+            if (typeof steps !== 'number')
+                steps = Math.max(6, Math.floor(6 * Math.pow(radius, 1/3) * (dist / (Math.PI))));
+
+            //ensure we have at least 3 steps..
+            steps = Math.max(steps, 3);
+
+            var f = dist / (steps),
+                t = start;
+
+            //modify direction
+            f *= clockwise ? -1 : 1;
+
+            for (var i=0; i<steps+1; i++) {
+                var cs = Math.cos(t),
+                    sn = Math.sin(t);
+
+                var nx = x + cs*radius,
+                    ny = y + sn*radius;
+
+                path.push([ nx, ny ]);
+
+                t += f
+            }
+            return path
+        }
     }
 
 });
