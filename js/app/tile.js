e9715ad6c15101c0f83f9647edf789eed55ddcb6
bonsai <LetBonsaiBe@gmail.com>
2016-09-08 17:04:55 +0800

    add line dash shader

--- js/app/tile.js
+++ js/app/tile.js
@@ -5,6 +5,7 @@ define(['./paint/SeaLevelPainter',
         './paint/LandLevelPainter',
         './paint/LineGeometry',
         './shader/LineShader',
+        './shader/LineDashShader',
         './data/SPPF',
         '../app/camera',
         '../app/control',
@@ -19,6 +20,7 @@ function(SeaLevelPainter,
          LandLevelPainter,
          LineGeometry,
          LineShader,
+         LineDashShader,
          ShapePointPromiseFactory,
          camera,
          control,
@@ -37,7 +39,9 @@ function(SeaLevelPainter,
         thickness: 20
     }));
 
-
+    var dashMat = new THREE.ShaderMaterial(LineDashShader({
+        side: THREE.DoubleSide
+    }));
 
     var time = 0.0;
 
@@ -51,6 +55,11 @@ function(SeaLevelPainter,
             var gui = new dat.GUI({
                 height : 5 * 32 - 1
             });
+
+            gui.add({Width: 10}, 'Width', 0, 20).onChange(function(value){mat.uniforms.thickness.value = value;});
+            gui.addColor({Color: [92,215,255]}, 'Color').onChange(function(value){mat.uniforms.diffuse.value = new THREE.Color(value[0]/255.0,value[1]/255.0,value[2]/255.0);});
+
+
 
             var tileGroup = new THREE.Object3D();
             scene.add(tileGroup);
@@ -67,14 +76,6 @@ function(SeaLevelPainter,
                         ]);
                     }
 
-                    // print something
-                    var printStr = '[';
-                    $.each(shapePath, function(idx, obj){
-                       printStr = printStr + '[' +(obj[0]/200.0)+','+(obj[1]/200.0)+ ']'+','
-                    });
-                    console.log(printStr+']');
-
-
                     var linkGeo = LineGeometry(shapePath, {distances: false, closed: false});
                     tileGroup.add(new THREE.Mesh(linkGeo, mat));
                 });
@@ -82,13 +83,25 @@ function(SeaLevelPainter,
 
             tileGroup.translateX(-1000);
             tileGroup.translateY(-1000);
+
+
+            // add dash line box
+            var boxPath = [[0, 0], [500, 0], [500, 500], [0, 500]];
+            var boxGeo = LineGeometry(boxPath, { distances: true, closed: true });
+
+            var mesh2 = new THREE.Mesh(boxGeo, dashMat);
+            scene.add(mesh2);
+
         },
 
         animate: function(){
             window.requestAnimationFrame( tile.animate );
             control.update();
             time += clock.getDelta();
-            mat.uniforms.thickness.value = Math.sin(time) * 20;
+
+            dashMat.uniforms.dashDistance.value = (Math.sin(time) / 2 + 0.5) * 0.5;
+            dashMat.uniforms.dashSteps.value = (Math.sin(Math.cos(time)) / 2 + 0.5) * 24;
+
             renderer.render(scene, camera);
         }
         
