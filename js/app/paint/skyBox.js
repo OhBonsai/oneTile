b4edf75457970d013915f240283ef0e6583c3f87
bonsai <LetBonsaiBe@gmail.com>
2016-09-19 17:40:30 +0800

    add sky box

---
+++ js/app/paint/skyBox.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Bonsai on 16-9-19.
+ */
+define(['three','when'],function(THREE,when){
+   'use strict';
+
+    var loader = new THREE.TextureLoader();
+
+
+    var createMaterial= function(path){
+        var texture = loader.load(path);
+        var material = new THREE.MeshBasicMaterial({map: texture, overdraw:0.5});
+        return material
+    };
+    
+    var skyMat = [
+        createMaterial('textures/tycho2t3_80_px.jpg'),
+        createMaterial('textures/tycho2t3_80_mx.jpg'),
+        createMaterial('textures/tycho2t3_80_py.jpg'),
+        createMaterial('textures/tycho2t3_80_my.jpg'),
+        createMaterial('textures/tycho2t3_80_pz.jpg'),
+        createMaterial('textures/tycho2t3_80_mz.jpg')
+    ];
+    
+    var skyBox = new THREE.Mesh(new THREE.BoxGeometry(30000,30000,30000,1,1,1),
+                                new THREE.MeshFaceMaterial(skyMat));
+    skyBox.scale.set(-1,1,1);
+    skyBox.eulerOrder = 'XZY';
+    skyBox.renderDepth = 200.0;
+    return skyBox
+});
--- js/app/tile.js
+++ js/app/tile.js
@@ -4,6 +4,7 @@
 define(['./paint/SeaLevelPainter',
         './paint/LandLevelPainter',
         './paint/LineGeometry',
+        './paint/skyBox',
         './shader/shader',
         './data/SPPF',
         './camera',
@@ -18,6 +19,7 @@ define(['./paint/SeaLevelPainter',
 function(SeaLevelPainter,
          LandLevelPainter,
          LineGeometry,
+         skyBox,
          shader,
          ShapePointPromiseFactory,
          camera,
@@ -44,8 +46,7 @@ function(SeaLevelPainter,
     var graMat = new THREE.ShaderMaterial(shader.line.LineGradientShader({
         side: THREE.DoubleSide
     }));
-
-    var domeMat =  new THREE.ShaderMaterial(shader.env.skyDomeShader);
+    
 
     var time = 0.0;
 
@@ -55,40 +56,7 @@ function(SeaLevelPainter,
             var llp = new LandLevelPainter();
             scene.add(slp.draw());
             scene.add(llp.draw());
-
-            // var gui = new dat.GUI({
-            //     height : 5 * 32 - 1
-            // });
-            //
-            // gui.add({Width: 10}, 'Width', 0, 20).onChange(function(value){mat.uniforms.thickness.value = value;});
-            // gui.addColor({Color: [92,215,255]}, 'Color').onChange(function(value){mat.uniforms.diffuse.value = new THREE.Color(value[0]/255.0,value[1]/255.0,value[2]/255.0);});
-            var gui_control = function () {
-
-                this.option1=false;
-                this.option2=false;
-                this.option3=false;
-
-
-            };
-
-            function gui()
-            {
-                var gui_controls=new gui_control();
-                var gui = new dat.GUI({autoPlace:true});
-                gui.add(gui_controls,'option1').onFinishChange(function(v){
-                    gui_controls.option2 = true;
-                    // Iterate over all controllers
-                    for (var i in gui.__controllers) {
-                        gui.__controllers[i].updateDisplay();
-                    }
-                });
-                gui.add(gui_controls,'option2');
-                gui.add(gui_controls,'option3');
-            }
-
-            gui();
-
-
+            
             var tileGroup = new THREE.Object3D();
             scene.add(tileGroup);
             var oneTileShapePointsPromise = ShapePointPromiseFactory.createPromise(13494, 6866);
@@ -107,27 +75,12 @@ function(SeaLevelPainter,
                     tileGroup.add(new THREE.Mesh(linkGeo, mat));
                 });
             });
-
             tileGroup.translateX(-1000);
             tileGroup.translateY(-1000);
-
-
-            // add dash line box
-            var boxPath = [[0, 0], [500, 0], [500, 500], [0, 500]];
-            var boxGeo = LineGeometry(boxPath, { distances: true, closed: true });
-
-            var mesh2 = new THREE.Mesh(boxGeo, graMat);
-            scene.add(mesh2);
-
-
-            // add skydome
-
-            var geometry = new THREE.SphereGeometry(30000, 200, 140);
-            var skyBox = new THREE.Mesh(geometry, domeMat);
-            skyBox.scale.set(-1, 1, 1);
-            skyBox.eulerOrder = 'XZY';
-            skyBox.renderDepth = 1000.0;
+            
             scene.add(skyBox);
+            
+
 
         },
 
@@ -135,13 +88,6 @@ function(SeaLevelPainter,
             window.requestAnimationFrame( tile.animate );
             control.update();
             time += clock.getDelta();
-
-            //console.log(time);
-
-            // dashMat.uniforms.dashDistance.value = (Math.sin(time) / 2 + 0.5) * 0.5;
-            dashMat.uniforms.dashSteps.value = (Math.sin(Math.cos(time)) / 2 + 0.5) * 24;
-            graMat.uniforms.time.value = time;
-
             renderer.render(scene, camera);
         }
         
