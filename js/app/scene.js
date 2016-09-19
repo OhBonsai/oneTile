a4951be2a4ec45fdea471a86bae142bd3bd92ec4
bonsai <LetBonsaiBe@gmail.com>
2016-09-19 17:43:17 +0800

    change fog parameter

--- js/app/scene.js
+++ js/app/scene.js
@@ -5,6 +5,6 @@ define( ["three"], function ( THREE ) {
     'use strict';
     
     var scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2( 0xcccccc, 0.0002 );
+    scene.fog = new THREE.FogExp2( 0xcccccc, 0.00002 );
     return scene;
 } );
