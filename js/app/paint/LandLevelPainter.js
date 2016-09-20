fad30370baf155c239c87cf1de3906a54ee4afd4
bonsai <LetBonsaiBe@gmail.com>
2016-09-20 20:05:15 +0800

    add grass material

--- js/app/paint/LandLevelPainter.js
+++ js/app/paint/LandLevelPainter.js
@@ -20,8 +20,16 @@ define(['three', './BasePaint'], function(THREE, BasePaint){
             var offsetY = option.offsetY || 0;
 
             var geometry = new THREE.PlaneGeometry(width, length);
-            var material = new THREE.MeshBasicMaterial({color: color, side: THREE.DoubleSide});
-            var plane = new THREE.Mesh(geometry, material);
+            // var material = new THREE.MeshBasicMaterial({color: color, side: THREE.DoubleSide});
+
+            var loader = new THREE.TextureLoader();
+            var groundTexture = loader.load( 'textures/grasslight-big.jpg' );
+            groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+            groundTexture.repeat.set( 25, 25 );
+            groundTexture.anisotropy = 16;
+            var groundMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111, map: groundTexture } );
+
+            var plane = new THREE.Mesh(geometry, groundMaterial);
             plane.translateX(offsetX);
             plane.translateY(offsetY);
             plane.translateZ(2);
