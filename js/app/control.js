b45d926f21fe4a44a9f26299b4f0c6dc8e0d4963
bonsai <LetBonsaiBe@gmail.com>
2016-09-24 17:36:52 +0800

    The DOM binded by TrackballControls from Document to Renederer.domElement

--- js/app/control.js
+++ js/app/control.js
@@ -1,9 +1,9 @@
 /**
  * Created by Bonsai on 16-8-17.
  */
-define( ["three", "../app/camera"], function( THREE, camera) {
+define( ["three", "../app/camera", "../app/renderer"], function( THREE, camera, renderer) {
     'use strict';
     
-    var control = new THREE.TrackballControls(camera);
+    var control = new THREE.TrackballControls(camera, renderer.domElement);
     return control;
 } );
