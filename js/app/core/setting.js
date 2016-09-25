2e45f34710d08cb29bc17a619568ce1a136a39d7
bonsai <LetBonsaiBe@gmail.com>
2016-09-25 19:40:21 +0800

    update setting

--- js/app/core/setting.js
+++ js/app/core/setting.js
@@ -4,20 +4,37 @@
 define([], function(){
     'use strict';
 
-    var CONST = {
+    var SETTING = {
         /** road shape painter **/
-        RSP : {
-            STANDARD_ROAD : 1
+        RSP: {
+            STANDARD_ROAD: 1
         },
 
         /** features init status **/
-        FIS : {
-            Link : true,
-            Node : false,
-            Build : false,
-            Name: true
+        FIS: {
+            Links: true,
+            Nodes: false,
+            Builds: false,
+            Names: true
+        },
+
+        /** 3D object Bonsai type **/
+        BTP: {
+            Link: 'Link',
+            Node: 'Node',
+            Name: 'Name',
+            Build: 'Build'
+        },
+
+        /** 3D object group name **/
+        GN: {
+            Links: 'Links',
+            Nodes: 'Nodes',
+            Names: 'Names',
+            Builds: 'Builds'
         }
+
     };
 
-    return CONST;
+    return SETTING;
 });
