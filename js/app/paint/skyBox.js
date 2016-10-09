d048bcb6c3a37eeb6cf1b7fbb0b13c691260b894
bonsai <LetBonsaiBe@gmail.com>
2016-10-09 21:52:25 +0800

    fit latest three version

--- js/app/paint/skyBox.js
+++ js/app/paint/skyBox.js
@@ -12,7 +12,7 @@ define(['three','when'],function(THREE,when){
         var material = new THREE.MeshBasicMaterial({map: texture, overdraw:0.5});
         return material
     };
-    
+
     var skyMat = [
         createMaterial('textures/tycho2t3_80_px.jpg'),
         createMaterial('textures/tycho2t3_80_mx.jpg'),
@@ -21,11 +21,11 @@ define(['three','when'],function(THREE,when){
         createMaterial('textures/tycho2t3_80_pz.jpg'),
         createMaterial('textures/tycho2t3_80_mz.jpg')
     ];
-    
+
     var skyBox = new THREE.Mesh(new THREE.BoxGeometry(30000,30000,30000,1,1,1),
                                 new THREE.MeshFaceMaterial(skyMat));
     skyBox.scale.set(-1,1,1);
-    skyBox.eulerOrder = 'XZY';
+    skyBox.rotation.order = 'XZY';
     skyBox.renderDepth = 200.0;
     return skyBox
 });
